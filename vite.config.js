bae1ff00478cbf966b5e79409df1e8491a1341b5
jcatgray <jill.griey@gmail.com>
2025-06-15 23:55:53 -0300

    implementado PWA

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,35 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [
+		sveltekit(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+			},
+			manifest: {
+				name: 'Meu App',
+				short_name: 'MeuApp',
+				description: 'Meu App PWA',
+				theme_color: '#ffffff',
+				background_color: '#ffffff',
+				display: 'standalone',
+				icons: [
+					{
+						src: 'android-chrome-192x192.png.png',
+						sizes: '192x192',
+						type: 'image/png'
+					},
+					{
+						src: 'android-chrome-512x512.png',
+						sizes: '512x512',
+						type: 'image/png'
+					}
+				]
+			}
+		})
+	]
 });
